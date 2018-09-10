8770befad7e86d1101ec9c83ac81c6c55675c2cd
argument100 <100blendy@gmail.com>
2018-09-11 08:02:12 +0900

    9-4. メソッド定義まで

--- deep_understand/9-Class.js
+++ deep_understand/9-Class.js
@@ -9,7 +9,7 @@ class Human911 {
 }
 
 let igarashi = new Human911('igarashi');
-igarashi.getName();
+// igarashi.getName();
 
 
 class Human921 {
@@ -24,7 +24,7 @@ class Human921 {
 }
 
 let igarashi2 = new Human921('igarashi', 35, 'M');
-igarashi2.showInfo();
+// igarashi2.showInfo();
 
 class Human931 {
   constructor(name, age, sex) {
@@ -41,4 +41,40 @@ class Human931 {
 }
 
 let igarashi3 = new Human931('igarashi', 35, 'M');
-igarashi3.showInfo();
+// igarashi3.showInfo();
+
+class Human941 {
+  constructor(fName, lName, age, sex) {
+    this.fName = fName;
+    this.lName = lName;
+    this.age = age;
+    this.sex = sex == 'M' ? '男性' : '女性';
+  }
+  getName() {
+    return this.lName + this.fName;
+  }
+  showInfo() {
+    console.log(`私は${getName}、${this.age}歳の${this.sex}です。`);
+  }
+}
+
+let igarashi4 = new Human941('hajime', 'igarashi', 35, 'M');
+// igarashi4.showInfo(); // エラー
+
+let count = 0;
+class Human942 {
+  constructor(name) {
+    this.name = name;
+    count += 1;
+  }
+  getName() {
+    console.log(`名前は${this.name}です`);
+  }
+  static getCount() {
+    return count;
+  }
+}
+
+let igarashi42 = new Human942('igarashi');
+let tarama42 = new Human942('tarama');
+console.log(Human942.getCount()); // 2
